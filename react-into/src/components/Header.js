2e2a959b1e19927f5d465f3fcd5a92eab8870fc9
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-06-29 17:33:38 -0700

    initial commit Co-Authored-By: Marney Mallory<marney.mallory@gmail.com> Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com>

--- react-into/src/components/Header.js
+++ react-into/src/components/Header.js
@@ -1,11 +1,9 @@
 import React from "react";
-import ticketsImage from "./../img/tickets.png";
 
 function Header(){
   return (
     <React.Fragment>
       <h1>Help Queue</h1>
-      <img src={ticketsImage} alt="An image of tickets" />
     </React.Fragment>
   );
 }
